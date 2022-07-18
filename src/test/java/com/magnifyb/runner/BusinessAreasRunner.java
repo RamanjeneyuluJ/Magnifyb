840dd5eb161175cdc0796714ff42bb0bea449190
DurgaUser <durgaradha7@gmail.com>
2022-07-18 12:02:12 +0530

    c

--- src/test/java/com/magnifyb/runner/BusinessAreasRunner.java
+++ src/test/java/com/magnifyb/runner/BusinessAreasRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Durga Parameswari\\Documents\\GitHub\\AutomationFramework\\src\\test\\java\\com\\magnifyb\\featurefile\\businessareas.feature", glue = "com.magnifyb.stepdefinition", plugin = { "pretty", "html:target/cucumber-report/businessAreas.html" }, monochrome = true)
+@CucumberOptions(features = "C:\\Users\\M0137\\git\\repository3\\AutomationFramework\\src\\test\\java\\com\\magnifyb\\featurefile\\businessareas.feature", glue = "com.magnifyb.stepdefinition", plugin = { "pretty", "html:target/cucumber-report/businessAreas.html" }, monochrome = true)
 
 public class BusinessAreasRunner {
 
